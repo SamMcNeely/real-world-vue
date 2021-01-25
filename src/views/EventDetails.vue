52c2d84c6e6aad6fe7cdd12558703525a56679a8
Sam McNeely <4legsam@gmail.com>
2021-01-24 21:31:09 -0800

    finished tutorial

---
+++ src/views/EventDetails.vue
@@ -0,0 +1,33 @@
+<template>
+  <div v-if="event">
+    <h1>{{ event.title }}</h1>
+    <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
+    <p>{{ event.description }}</p>
+  </div>
+</template>
+
+<script>
+import EventService from '@/services/EventService.js'
+
+export default {
+  props: {
+    id: String
+  },
+  data() {
+    return {
+      event: null
+    }
+  },
+  created() {
+    EventService.getEvent(this.id)
+      .then(Response => {
+        this.event = Response.data
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+}
+</script>
+
+<style></style>
